805b2096d73af8f2ad077db73a20a8ce85e3864c
Bryson Creighton <brysoncreighton@gmail.com>
2020-08-01 21:59:42 -0600

    MV front-end files to app folder
    Move frontend files to noteful-app folder for to keep a clear separation of files from backend and front end.

--- noteful-server/src/app.js
+++ noteful-server/src/app.js
@@ -16,7 +16,7 @@ app.use(helmet())
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.send('Hello, boilerplate!')
+  res.send('Hello, Noteful!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
