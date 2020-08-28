18884b2c537cd20fa20a3e06a6dbf5515118fc29
Bryson Creighton <brysoncreighton@gmail.com>
2020-08-27 22:37:16 -0600

    Update EditNote.js
    Implement form; and componentdidmount function to pull the appropriate note information in the form fields.
    
    Next steps needed: implmenet PATCH  upon submition.

--- noteful-app/src/components/editNote/EditNote.js
+++ noteful-app/src/components/editNote/EditNote.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import ValidationError from '../ValidationError'
 import NotefulContext from '../../NotefulContext'
 
 const Required = () => {
-    <span className='AddFolderRequired'>*</span>
+    <span className='EditFolderRequired'>*</span>
 }
 
 class EditNote extends Component {
@@ -56,7 +56,7 @@ class EditNote extends Component {
     validateNoteTitle() {
         const noteTitle = this.state.noteTitle.value.trim();
 
-        if(noteTitle.length === 0) {
+        if (noteTitle.length === 0) {
             return "Note title is required";
         } else if (noteTitle.length < 3) {
             return "Note title must be at least 3 characters long";
@@ -66,7 +66,7 @@ class EditNote extends Component {
     ValidateNoteFolder() {
         const noteFolder = this.state.noteFolder.value.trim();
 
-        if(noteFolder.length === 0) {
+        if (noteFolder.length === 0) {
             return "Please select a folder; it is required";
         }
     }
@@ -74,9 +74,9 @@ class EditNote extends Component {
     ValidateNoteContent() {
         const noteContent = this.state.noteContent.value.trim();
 
-        if(noteContent.length === 0) {
+        if (noteContent.length === 0) {
             return "Content is required";
-        } else if(noteContent.length < 5) {
+        } else if (noteContent.length < 5) {
             return "Please enter at least 5 characters";
         }
     }
@@ -92,10 +92,10 @@ class EditNote extends Component {
         ))
     }
 
-    getFolderId = (noteFolder) => {
+    getFolderName = (noteFolderId) => {
         return this.context.folders.find(folder => {
-            return noteFolder === folder.name
-        }).id
+            return noteFolderId === folder.id
+        }).name
     }
 
     handleSubmit = () => {
@@ -104,5 +104,114 @@ class EditNote extends Component {
 
     handleClickCancel = () => {
         this.props.history.push('/')
+    }
+
+    componentDidMount() {
+        const noteId = this.props.match.params.noteId
+
+        fetch(`https://localhost:8000/api/notes/${noteId}`, {
+            method: 'GET'
+        })
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(res.status)
+                }
+
+                return res.json()
+            })
+            .then(note => {
+                this.setState({
+                    noteTitle: note.name,
+                    noteFolder: note.folder_id,
+                    noteContent: note.content,
+                })
+            })
+            .catch(error => this.setState({ error }))
+    }
+
+    render() {
+        const noteTitleError = this.validateNoteTitle();
+        const noteFolderError = this.validateNoteFolder();
+        const noteContentError = this.validateNoteContent();
+        const { error } = this.state;
+        const folderList = this.generateFolderDDList();
+
+        const { noteTitle, noteFolder, noteContent } = this.state
+
+        return (
+            <form
+                className='EditNoteForm'
+                onSubmit={this.handleSubmit}
+            >
+                <div className='EditNoteError' role='alert'>
+                    {error && <p>{error.message}</p>}
+                </div>
+                <div>
+                    <label htmlFor='noteTitle'>
+                        Note Title
+                  {' '}
+                        <Required />
+                    </label>
+                    <input
+                        type='text'
+                        name='noteTitle'
+                        id='noteTitle'
+                        placeholder='Enter note title'
+                        onChange={e => this.updateNoteTitle(e.target.value)}
+                        required
+                        value={noteTitle}
+                    />
+                    {this.state.noteTitle.touched && (
+                        <ValidationError message={noteTitleError} />
+                    )}
+                </div>
+                <div>
+                    <label htmlFor='noteFolder'>
+                        Note Folder
+                  {' '}
+                        <Required />
+                    </label>
+                    <select
+                        id='noteFolder'
+                        name='noteFolder'
+                        onChange={e => this.updateNoteFolder(e.target.value)}
+                        required
+                        selected={this.getFolderName(noteFolder)}
+                    >
+                        {folderList}
+                    </select>
+                    {this.state.noteFolder.touched && (
+                        <ValidationError message={noteFolderError} />
+                    )}
+                </div>
+                <div>
+                    <label htmlFor='noteContent'>
+                        Content
+                  {' '}
+                        <Required />
+                    </label>
+                    <textarea
+                        name='noteContent'
+                        id='noteContent'
+                        placeholder="Enter your note content here..."
+                        onChange={e => this.updateNoteContent(e.target.value)}
+                        required
+                        value={noteContent}
+                    ></textarea>
+                    {this.state.noteContent.touched && (
+                        <ValidationError message={noteContentError} />
+                    )}
+                </div>
+                <div className='EditNoteButtons'>
+                    <button type='button' onClick={this.handleClickCancel}>
+                        Cancel
+                </button>
+                    {' '}
+                    <button type='submit'>
+                        Save
+                </button>
+                </div>
+            </form>
+        )
     }
 }
